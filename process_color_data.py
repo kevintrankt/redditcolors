f09eebb9930b22cca65eb9cb00f35b9a07fbcedf
kevintrankt <kevin.tran.kt@gmail.com>
2020-08-12 12:47:11 -0700

    messing around with different color sorting algorithms

--- process_color_data.py
+++ process_color_data.py
@@ -1,8 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 from os import listdir
 from os.path import isfile, join
 from collections import defaultdict
 import pickle
+import math
+import colorsys
 
 PATH = 'resized'
 files = [f for f in listdir(PATH) if isfile(join(PATH, f))]
@@ -22,7 +24,35 @@ if load_backup == 'y':
     print(image_colors)
     with open('image_colors.pkl', 'wb') as f:
         pickle.dump(image_colors, f)
+
+    image_colors_list = []
+    for key in image_colors:
+        if type(key) is tuple:
+            # remove 4th tuple value
+            if len(key) > 3:
+                rgb = key[:3]
+            else:
+                rgb = key
+            for i in range(0, image_colors[key]):
+                image_colors_list.append(rgb)
+
+    with open('image_colors_list.pkl', 'wb') as f:
+        pickle.dump(image_colors_list, f)
+
 else:
     with open('image_colors.pkl', 'rb') as f:
         image_colors = pickle.load(f)
-    print(image_colors)
+    with open('image_colors_list.pkl', 'rb') as f:
+        image_colors_list = pickle.load(f)
+
+print(image_colors_list)
+print(len(image_colors_list))
+
+
+# print(image_colors)
+
+# image_dim = math.floor(math.sqrt(len(image_colors)))
+# im = Image.new("RGB", (image_dim,image_dim), "white")
+#
+#
+# im.show()
